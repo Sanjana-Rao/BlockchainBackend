295c0c52fc7dca65b8b8d2120b8805274a2e58ab
unknown <rmsanjanarao@gmail.com>
2022-05-25 17:25:10 +0530

    first

--- src/js/app.js
+++ src/js/app.js
@@ -3,41 +3,36 @@ App = {
   contracts: {},
   account: '0x0',
   hasVoted: false,
+  closingTime: {
+    year: 2022,
+    month: 4,
+    date: 23,
+    hour: 00,
+    minutes: 35,
+    seconds: 0,
+    milliseconds: 0
+  },
 
-  init: function () {
+  init: function() {
     return App.initWeb3();
   },
 
-  initWeb3: function () {
+  initWeb3: function() {
     // TODO: refactor conditional
     if (typeof web3 !== 'undefined') {
       // If a web3 instance is already provided by Meta Mask.
-      const ethEnabled = () => {
-        if (window.ethereum) {
-          window.web3 = new Web3(window.ethereum);
-          return true;
-        }
-        return false;
-      };
-      if (!ethEnabled()) {
-        alert(
-          'Please install an Ethereum-compatible browser or extension like MetaMask to use this dApp!'
-        );
-      }
-      web3 = window.web3;
       App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
     } else {
       // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider(
-        'http://localhost:7545'
-      );
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
     }
     return App.initContract();
   },
 
-  initContract: function () {
-    $.getJSON('Election.json', function (election) {
+  initContract: function() {
+    $.getJSON("Election.json", function(election) {
       // Instantiate a new truffle contract from the artifact
       App.contracts.Election = TruffleContract(election);
       // Connect provider to interact with contract
@@ -50,123 +45,150 @@ App = {
   },
 
   // Listen for events emitted from the contract
-  listenForEvents: function () {
-    App.contracts.Election.deployed().then(function (instance) {
+  listenForEvents: function() {
+    App.contracts.Election.deployed().then(function(instance) {
       // Restart Chrome if you are unable to receive this event
       // This is a known issue with Metamask
       // https://github.com/MetaMask/metamask-extension/issues/2393
-      instance
-        .votedEvent(
-          {},
-          {
-            fromBlock: 0,
-            toBlock: 'latest',
-          }
-        )
-        .watch(function (error, event) {
-          console.log('event triggered', event);
-          // Reload when a new vote is recorded
-          App.render();
-        });
+      instance.votedEvent({}, {
+        fromBlock: 0,
+        toBlock: 'latest'
+      }).watch(function(error, event) {
+        console.log("event triggered", event)
+        // Reload when a new vote is recorded
+        App.render();
+      });
     });
   },
 
-  render: async () => {
+
+  displayTime: function() {
+    var today = new Date();
+    var closingDate = new Date(App.closingTime.year, App.closingTime.month, App.closingTime.date, 
+      App.closingTime.hour, App.closingTime.minutes, App.closingTime.seconds, 
+      App.closingTime.milliseconds);
+    
+    $("#ClosingTime").html('Voting ends at : ' + closingDate);
+
+    var remSeconds = Math.max(0, Math.floor((closingDate.getTime() - today.getTime()) / 1000));
+    var remDays = Math.floor(remSeconds / (60 * 60 * 24));
+    remSeconds -= remDays * 60 * 60 * 24;
+
+    var remHours = Math.floor(remSeconds / (60*60));
+    remSeconds -= remHours * 60 * 60;
+
+    var remMinutes = Math.floor(remSeconds / 60);
+    remSeconds -= remMinutes * 60;
+
+    $("#RemainingTime").html('Time left until closing : ' + remDays + ' Day(s), ' + 
+      remHours + ' Hour(s), ' + remMinutes + ' Minute(s), ' + remSeconds + ' Second(s)');
+
+    var refresh=1000; // Refresh rate in milli seconds
+    mytime=setTimeout('App.displayTime()',refresh)
+  },
+
+  render: function() {
     var electionInstance;
-    var loader = $('#loader');
-    var content = $('#content');
+    var loader = $("#loader");
+    var content = $("#content");
+    var closingDate = new Date(App.closingTime.year, App.closingTime.month, App.closingTime.date, 
+      App.closingTime.hour, App.closingTime.minutes, App.closingTime.seconds, 
+      App.closingTime.milliseconds);
+      var now = new Date();
 
     loader.show();
-    content.hide();
+    content.hide
+    $('#Title').html('Cast your vote!');
+    if(now < closingDate)
+      $('#Votes').hide();
+    else {
+      $('#Votes').show();
+      $('#Title').html('Election Results');
+      $('form').hide();
+    }
+    App.displayTime();
 
     // Load account data
-    try {
-      const accounts = await window.ethereum.request({
-        method: 'eth_requestAccounts',
-      });
-      App.account = accounts[0];
-      $('#accountAddress').html('Your Account: ' + App.account);
-    } catch (error) {
-      if (error.code === 4001) {
-        // User rejected request
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
       }
-      console.log(error);
-    }
+    });
 
     // Load contract data
-    App.contracts.Election.deployed()
-      .then(function (instance) {
-        electionInstance = instance;
-        return electionInstance.candidatesCount();
-      })
-      .then(async (candidatesCount) => {
-        const promise = [];
-        for (var i = 1; i <= candidatesCount; i++) {
-          promise.push(electionInstance.candidates(i));
-        }
+    App.contracts.Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      return electionInstance.candidatesCount();
+    }).then(function(candidatesCount) {
+      // Store all promised to get candidate info
+      const promises = [];
+      for (var i = 1; i <= candidatesCount; i++) {
+        promises.push(electionInstance.candidates(i));
+      }
 
-        const candidates = await Promise.all(promise);
-        var candidatesResults = $('#candidatesResults');
+      // Once all candidates are received, add to dom
+      Promise.all(promises).then((candidates) => {
+        var candidatesResults = $("#candidatesResults");
         candidatesResults.empty();
 
         var candidatesSelect = $('#candidatesSelect');
         candidatesSelect.empty();
 
-        for (var i = 0; i < candidatesCount; i++) {
-          var id = candidates[i][0];
-          var name = candidates[i][1];
-          var voteCount = candidates[i][2];
+        candidates.forEach(candidate => {
+          var id = candidate[0];
+          var name = candidate[1];
+          var voteCount = candidate[2];
 
           // Render candidate Result
-          var candidateTemplate =
-            '<tr><th>' +
-            id +
-            '</th><td>' +
-            name +
-            '</td><td>' +
-            voteCount +
-            '</td></tr>';
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td>";
+          if(now < closingDate) {
+            candidateTemplate += "</tr>";
+          }
+          else {
+            candidateTemplate += "<td>" + voteCount + "</td></tr>";
+          }
           candidatesResults.append(candidateTemplate);
 
           // Render candidate ballot option
-          var candidateOption =
-            "<option value='" + id + "' >" + name + '</ option>';
-          candidatesSelect.append(candidateOption);
-        }
-        return electionInstance.voters(App.account);
-      })
-      .then(function (hasVoted) {
-        // Do not allow a user to vote
-        if (hasVoted) {
-          $('form').hide();
-        }
-        loader.hide();
-        content.show();
-      })
-      .catch(function (error) {
-        console.warn(error);
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidatesSelect.append(candidateOption);          
+        })
       });
+
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted) {
+      // Do not allow a user to vote
+      if(hasVoted) {
+        $('form').hide();
+        if(now < closingDate)
+          $("#Title").html('Thank you for casting your vote!');
+          $("#Byline").html('Remove your Metamask account and logout!');      
+      }
+      loader.hide();
+      content.show();
+    }).catch(function(error) {
+      console.warn(error);
+    });
   },
 
-  castVote: function () {
+  castVote: function() {
     var candidateId = $('#candidatesSelect').val();
-    App.contracts.Election.deployed()
-      .then(function (instance) {
-        return instance.vote(candidateId, { from: App.account });
-      })
-      .then(function (result) {
-        // Wait for votes to update
-        $('#content').hide();
-        $('#loader').show();
-      })
-      .catch(function (err) {
-        console.error(err);
-      });
-  },
+
+    App.contracts.Election.deployed().then(function(instance) {
+      return instance.vote(candidateId, { from: App.account });
+    }).then(function(result) {
+      // Wait for votes to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
+    });
+  }
 };
 
-$(function () {
-  $(window).load(function () {
+$(function() {
+  $(window).load(function() {
     App.init();
   });
 });
